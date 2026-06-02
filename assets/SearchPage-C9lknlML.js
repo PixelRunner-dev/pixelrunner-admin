e6d3e76c3a7d00d77716e86679cb1d69ded2336f
PMK <PMK@users.noreply.github.com>
2026-06-02 23:26:10 +0000

    deploy: 52ee3735000580f01dab1d118900b966d4ae2cdb

---
+++ assets/SearchPage-C9lknlML.js
@@ -0,0 +1,1 @@
+import{Et as e,G as t,J as n,Q as r,X as i,ct as a}from"./index-CdfDE5dJ.js";import{t as o}from"./LibrarySearch-B6WFCK1t.js";var s=r({__name:`SearchPage`,setup(r){return(r,s)=>(a(),n(`main`,null,[s[0]||=t(`h1`,null,`Library Search`,-1),i(o),t(`p`,null,`Searching for: `+e(r.$route.query.q),1)]))}});export{s as default};
