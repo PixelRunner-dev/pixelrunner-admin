37584a60e145101ee8da7d5e4f525212019e1fe2
PMK <PMK@users.noreply.github.com>
2026-06-18 10:26:44 +0000

    deploy: 4408f648600f46019c84673da82435cadf447b72

---
+++ assets/UpdatePage-Bc1zZAeh.js
@@ -0,0 +1,1 @@
+import{A as e,Et as t,G as n,J as r,ct as i}from"./index-SzvR4Xti.js";var a={};function o(e,a){return i(),r(`main`,null,[n(`h1`,null,t(e.$t(`updatePage.title`)),1)])}var s=e(a,[[`render`,o]]);export{s as default};
